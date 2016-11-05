03f9bb2d1eeec76ef76c4f28fec681a5b7060298
chrislu1207 <krizloo@gmail.com>
2016-11-05 08:42:01 -0700

    Refactor binarySearchTree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -12,23 +12,16 @@ var BSTMethods = {};
 BSTMethods.insert = function(value) { //O(log n)
 
   var insertNode = function(node) {
-    //console.log('Currently at node', node.value);
     if (value < node.value) {
-      //console.log(value, 'is less than', node.value);
       if (node.left.value === undefined) {
-        //console.log('No left node defined, creating');
         node.left = BinarySearchTree(value);
       } else {
-        //console.log('Traversing left from', node.value);
         insertNode(node.left);
       }
     } else if (value > node.value) {
-      //console.log(value, 'is greater than', node.value);
       if (node.right.value === undefined) {
-        //console.log('No right node defined, creating');
         node.right = BinarySearchTree(value);
       } else {
-        //console.log('Traversing right from', node.value);
         insertNode(node.right);
       }
     }
@@ -56,17 +49,13 @@ BSTMethods.contains = function(value) { //O(log n)
 
 BSTMethods.depthFirstLog = function(callback) { //O(log n)
   var depthFirstTravel = function(node) {
-    //console.log('Executing callback on node', node.value);
     callback(node.value);
     if (node.left.value !== undefined) {
-      //console.log('Traversing down left to node', node.left.value);
       depthFirstTravel(node.left);
     }
     if (node.right.value !== undefined) {
-      //console.log('Traversing down right to node', node.right.value);
       depthFirstTravel(node.right);
     }
-    //console.log('No children found, returning');
     return;
   };
   depthFirstTravel(this);
